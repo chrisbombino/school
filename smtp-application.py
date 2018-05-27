8d66e3478eea234bf0c631071e0479c077028c82
chrisbombino <32116781+chrisbombino@users.noreply.github.com>
2018-05-27 13:24:00 -0400

    added auth file

--- smtp-application.py
+++ smtp-application.py
@@ -1,10 +1,12 @@
 import smtplib
+from . import auth
 
 from_email = 'cjbombino@gmail.com'
 to_email = from_email
+password = auth.password
 
 server = smtplib.SMTP('smtp.gmail.com:587')
 
 server.ehlo()
 server.starttls()
-server.login(u, p)
+server.login(from_email, password)
