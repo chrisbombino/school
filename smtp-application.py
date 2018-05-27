6c6ef02c72b43c75e08c77e889902b9f8e49aca6
chrisbombino <32116781+chrisbombino@users.noreply.github.com>
2018-05-27 14:33:29 -0400

    formatting buttons & created close pw window function

--- smtp-application.py
+++ smtp-application.py
@@ -3,6 +3,9 @@ import auth
 from email.mime.text import MIMEText
 from tkinter import *
 
+def close_pw_window():
+    pw_window.destroy()
+
 top = Tk()
 tk_to = Label(top, text='To:')
 tk_to.pack()
@@ -21,24 +24,29 @@ E3.pack()
 
 tk_body = Label(top, text='Body:')
 tk_body.pack()
-E4 = Entry(top, bd=5)
+E4 = Text(top, bd=5, height=10)
 E4.pack()
 
-send_button = Button(top, text='Send')
+send_button = Button(top, text='Send', height=2, padx=50)
 send_button.pack()
 #top.mainloop()
 
 pw_window = Tk()
-pw_label = Label(pw_window, text='Password:')
+
+pw_label = Label(pw_window, text='Password:', height=2)
 pw_label.pack()
-E5 = Entry(pw_window, bd=5)
+E5 = Entry(pw_window, bd=5, show='*')
 E5.pack()
 
-pw_submit = Button(pw_window, text='Submit')
+pw_submit = Button(pw_window, text='Submit', height=2, width=10)
 pw_submit.pack()
-pw_cancel = Button(pw_window, text='Cancel')
+
+pw_cancel = Button(pw_window, text='Cancel', command=close_pw_window, height=2, width=10)
 pw_cancel.pack()
+
 pw_window.mainloop()
+
+
 '''
 # initialize message info
 from_email = 'cjbombino@gmail.com'
