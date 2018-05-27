247b52df75c9bbb26b390eb0d091b8af6f27a561
chrisbombino <32116781+chrisbombino@users.noreply.github.com>
2018-05-27 13:38:08 -0400

    added message info

--- smtp-application.py
+++ smtp-application.py
@@ -1,12 +1,24 @@
 import smtplib
-from . import auth
+import auth
 
 from_email = 'cjbombino@gmail.com'
 to_email = from_email
-password = auth.password
+password = auth.app_password
 
 server = smtplib.SMTP('smtp.gmail.com:587')
 
 server.ehlo()
 server.starttls()
 server.login(from_email, password)
+
+subject = 'Test'
+message = 'Hi Mom'
+
+msg = '\r\n'.join([
+    'Subject: Test',
+    '',
+    message
+])
+print(msg)
+server.sendmail(from_email, to_email, message)
+server.quit()
